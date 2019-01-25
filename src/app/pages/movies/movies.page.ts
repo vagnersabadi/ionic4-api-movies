b3fdeaa245bb661a1b19d8d042a6a98780f2b922
Vagner Sabadi <vagnerbuzatta@hotmail.com>
2019-01-25 16:02:08 -0200

    Remove Key

---
+++ src/app/pages/movies/movies.page.ts
@@ -0,0 +1,30 @@
+import { MovieService, SearchType } from './../../services/movie.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.page.html',
+  styleUrls: ['./movies.page.scss'],
+})
+export class MoviesPage implements OnInit {
+
+  results: Observable<any>;
+  // tslint:disable-next-line:no-inferrable-types
+  searchTerm: string = '';
+  type: SearchType = SearchType.all;
+
+  /**
+   * Constructor of our first page
+   * @param movieService The movie Service to get data
+   */
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit() { }
+
+  searchChanged() {
+    // Call our service function which returns an Observable
+    this.results = this.movieService.searchData(this.searchTerm, this.type);
+  }
+}
